5a4c91e134cc1fe423569ccb0fb3ee28d83bd27b
Dimitrije Stanković <diki.zajecar@gmail.com>
2025-07-05 18:25:06 +0200

    napravljen c# projekat

---
+++ PRMuIS_Kviskoteka/PRMuIS_Kviskoteka/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PRMuIS_Kviskoteka
+{
+ 
+    //PROJEKAT 25 - KVISKOTEKA
+    //ČLANOVI TIMA: Dimitrije Stanković PR81/2022
+    //              Vojin Jovanović PR82/2022
+    //FTN, ŠKOLSKA 2024/25.
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
